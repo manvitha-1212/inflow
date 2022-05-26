e59b6de6371782381476bcfb78c6927feb91c5d8
manvitha-1212 <67196221+manvitha-1212@users.noreply.github.com>
2022-05-26 22:35:57 +0530

    Update happiness_streamlit.py

--- happiness_streamlit.py
+++ happiness_streamlit.py
@@ -19,12 +19,12 @@ from PIL import Image
 
 reload_model = joblib.load('happiness_model')
 
-GDP_per_capita= st.numeric_input('GDP_per_capita')
-Social_support = st.numeric_input('Social_support') 
-Healthy_life_expectancy= st.numeric_input("Healthy_life_expectancy") 
-Freedom_to_make_life_choices= st.numeric_input('Freedom_to_make_life_choices')
-Generosity = st.numeric_input('Generosity') 
-Perceptions_of_corruption= st.numeric_input("Perceptions_of_corruption") 
+GDP_per_capita= st.number_input('GDP_per_capita')
+Social_support = st.number_input('Social_support') 
+Healthy_life_expectancy= st.number_input("Healthy_life_expectancy") 
+Freedom_to_make_life_choices= st.number_input('Freedom_to_make_life_choices')
+Generosity = st.number_input('Generosity') 
+Perceptions_of_corruption= st.number_input("Perceptions_of_corruption") 
 result =""
 
 if st.button("Predict"): 
